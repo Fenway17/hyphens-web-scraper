7db38f985d0bbe9b85de349b78a6ee41dd8229bd
Tan Yong Rui <tanyongruifenway@gmail.com>
2024-06-13 14:28:18 +0800

    Reformat http request into a separate file. Add extra http request method for location id specific data.

--- src/api-request.js
+++ src/api-request.js
@@ -3,23 +3,23 @@ require("dotenv").config({ path: "./.env" });
 const fs = require("fs").promises;
 const JsonWriter = require("./json-writer");
 const JsonToCsv = require("./json-to-csv");
-
-const API_URL = process.env.API_URL;
-const HEADER_ORIGIN = process.env.HEADER_ORIGIN;
-const HEADER_REFERER = process.env.HEADER_REFERER;
-const HEADER_X_API_KEY = process.env.HEADER_X_API_KEY;
+const httpRequest = require("./http-request");
 
 const jsonFilePath = "data.json";
 const csvFilePath = "data.csv";
 
-const PAGE_SIZE = 10; // too large leads to error 502
+const PAGE_SIZE = 1000; // too large leads to error 502
 const CATEGORY_ID = 61; // for outpatient medical service
 
 (async function main() {
-    let pageNum = 3;
+    let pageNum = 1;
     while (true) {
         // packet example in api-request-data-example.json
-        let packetData = await fetchData(pageNum, PAGE_SIZE, CATEGORY_ID);
+        let packetData = await httpRequest.fetchListData(
+            pageNum,
+            PAGE_SIZE,
+            CATEGORY_ID
+        );
         // console.log("- packetData:");
         // console.log(packetData); // check data
         let resultsList = packetData["Results"];
@@ -34,12 +34,14 @@ const CATEGORY_ID = 61; // for outpatient medical service
             console.log("- checking results");
             // console.log("- result:");
             // console.log(result);
-            centreData = {};
-            for (const key in result) {
-                centreData[key] = result[key];
-            }
+
+            // copy data over
+            centreData = result;
+
             let HCICode = result["HciCode"];
             // console.log(HCICode);
+
+            // reformat operation hours
             let operationalHours = {};
             if (
                 result["OperatingHour"] != null &&
@@ -56,16 +58,23 @@ const CATEGORY_ID = 61; // for outpatient medical service
             }
             centreData["OperatingHour"] = operationalHours;
 
-            // // console.log(operationalHours);
-            // let specifiedServices = [];
-            // if (
-            //     result["SpcServices"] != null &&
-            //     result["SpcServices"].length > 0
-            // ) {
-            //     console.log(result["SpcServices"]);
-            //     specifiedServices = result["SpcServices"].split(",");
-            // }
-            // console.log(specifiedServices);
+            // reformat specified services
+            let specifiedServices = [];
+            if (
+                result["SpcServices"] != null &&
+                result["SpcServices"].length > 0 &&
+                result["LocationId"] != null
+            ) {
+                console.log(result["SpcServices"]);
+                let locationPacketData = await httpRequest.fetchLocationData(
+                    CATEGORY_ID,
+                    result["LocationId"]
+                );
+                let locationResult = locationPacketData["Results"];
+                specifiedServices = locationResult["SpcServices"].split(",");
+                centreData["SpcServices"] = specifiedServices;
+            }
+            console.log(specifiedServices);
 
             if (HCICode != undefined) {
                 // clinic info key is HCICode, a UUID
@@ -77,9 +86,10 @@ const CATEGORY_ID = 61; // for outpatient medical service
             }
         }
 
-        break;
+        // break
         pageNum++; // go to next page
     }
+
     // convert json to csv
     const jsonData = await fs.readFile(jsonFilePath, "utf8");
     const jsonDict = JSON.parse(jsonData);
@@ -93,44 +103,3 @@ const CATEGORY_ID = 61; // for outpatient medical service
     }
     await JsonToCsv.writeJsonObjectToCsv(jsonList, csvFilePath);
 })();
-
-// fetch data through http request and return it
-async function fetchData(pageNum, pageSize, categoryId) {
-    console.log("- fetching data...");
-    const options = {
-        method: "GET",
-        url: API_URL,
-        params: {
-            pageNum: pageNum,
-            pageSize: pageSize,
-            categoryId: categoryId,
-        },
-        headers: {
-            Accept: "application/json, text/plain, */*",
-            "Accept-Encoding": "gzip, deflate, br, zstd",
-            "Accept-Language": "en-US,en;q=0.9",
-            Origin: HEADER_ORIGIN,
-            Referer: HEADER_REFERER,
-            "Sec-Ch-Ua":
-                '"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"',
-            "Sec-Ch-Ua-Mobile": "?0",
-            "Sec-Ch-Ua-Platform": '"Windows"',
-            "Sec-Fetch-Dest": "empty",
-            "Sec-Fetch-Mode": "cors",
-            "Sec-Fetch-Site": "same-site",
-            "User-Agent":
-                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
-            "X-Api-Key": HEADER_X_API_KEY,
-            "X-Hh-Agent": "portal-hhngreact",
-        },
-    };
-
-    try {
-        let response = await axios.request(options);
-        console.log("- response data successfully retrieved.");
-        return response.data;
-    } catch (error) {
-        console.error("Error:", error.message);
-        throw error;
-    }
-}
---
+++ src/http-request.js
@@ -0,0 +1,98 @@
+const axios = require("axios");
+require("dotenv").config({ path: "./.env" });
+
+const API_URL_LOCATION_LIST = process.env.API_URL_LOCATION_LIST;
+const API_URL_LOCATION = process.env.API_URL_LOCATION;
+const HEADER_ORIGIN = process.env.HEADER_ORIGIN;
+const HEADER_REFERER = process.env.HEADER_REFERER;
+const HEADER_X_API_KEY = process.env.HEADER_X_API_KEY;
+
+// fetch data through http request and return it
+async function fetchListData(pageNum, pageSize, categoryId) {
+    console.log("- fetching list data...");
+    const options = {
+        method: "GET",
+        url: API_URL_LOCATION_LIST,
+        params: {
+            pageNum: pageNum,
+            pageSize: pageSize,
+            categoryId: categoryId,
+        },
+        headers: {
+            Accept: "application/json, text/plain, */*",
+            "Accept-Encoding": "gzip, deflate, br, zstd",
+            "Accept-Language": "en-US,en;q=0.9",
+            Origin: HEADER_ORIGIN,
+            Referer: HEADER_REFERER,
+            "Sec-Ch-Ua":
+                '"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"',
+            "Sec-Ch-Ua-Mobile": "?0",
+            "Sec-Ch-Ua-Platform": '"Windows"',
+            "Sec-Fetch-Dest": "empty",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "same-site",
+            "User-Agent":
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+            "X-Api-Key": HEADER_X_API_KEY,
+            "X-Hh-Agent": "portal-hhngreact",
+        },
+    };
+
+    try {
+        let response = await axios.request(options);
+        console.log("- list response data successfully retrieved.");
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching data:", error.message);
+        throw error;
+    }
+}
+
+("?categoryId=61&locationId=110707");
+
+async function fetchLocationData(categoryId, locationId) {
+    console.log("- fetching location data...");
+    // Define the URL and query parameters
+    const options = {
+        method: "GET",
+        url: API_URL_LOCATION,
+        params: {
+            categoryId: categoryId,
+            locationId: locationId,
+        },
+        headers: {
+            Accept: "application/json, text/plain, */*",
+            "User-Agent":
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+            "X-Api-Key": HEADER_X_API_KEY,
+            "X-Hh-Agent": "portal-hhngreact",
+            "Sec-Fetch-Dest": "empty",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "same-site",
+            Referer: HEADER_REFERER,
+            Origin: HEADER_ORIGIN,
+            "Sec-Ch-Ua":
+                '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
+            "Sec-Ch-Ua-Mobile": "?0",
+            "Sec-Ch-Ua-Platform": '"Windows"',
+            "Accept-Encoding": "gzip, deflate, br, zstd",
+            "Accept-Language": "en-US,en;q=0.9",
+            Priority: "u=1, i",
+        },
+    };
+
+    // Axios request with axios.request
+    try {
+        let response = await axios.request(options);
+        console.log("- location response data successfully retrieved.");
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching data:", error.message);
+        throw error;
+    }
+}
+
+module.exports = {
+    fetchListData,
+    fetchLocationData,
+};
